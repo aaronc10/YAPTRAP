fe6141ddf877d3a04e1affd3e8dff97b7656b5a5
Aaron Connolly <aaronconnolly12@gmail.com>
2024-11-27 12:28:46 +0100

    Removed unused API

--- app/api/eleven-labs/route.js
+++ app/api/eleven-labs/route.js
@@ -1,33 +1,33 @@
-import { ElevenLabsClient } from "elevenlabs";
-import { NextResponse } from "next/server";
+// import { ElevenLabsClient } from "elevenlabs";
+// import { NextResponse } from "next/server";
 
-const elevenLabs = new ElevenLabsClient({
-  apiKey: process.env.NEXT_PUBLIC_ELEVEN_LABS_API_KEY,
-});
+// const elevenLabs = new ElevenLabsClient({
+//   apiKey: process.env.NEXT_PUBLIC_ELEVEN_LABS_API_KEY,
+// });
 
-export async function POST(request) {
-  try {
-    const { text } = await request.json();
-    const audioStream = await elevenLabs.textToSpeech.convertAsStream("1TE7ou3jyxHsyRehUuMB", {
-      optimise_streaming_latency: 0,
-      output_format: "mp3_22050_32",
-      text: text,
-      voice_settings: {
-        stability: 0.5,
-        similarity_boost: 0.5
-      }
-    });
+// export async function POST(request) {
+//   try {
+//     const { text } = await request.json();
+//     const audioStream = await elevenLabs.textToSpeech.convertAsStream("1TE7ou3jyxHsyRehUuMB", {
+//       optimise_streaming_latency: 0,
+//       output_format: "mp3_22050_32",
+//       text: text,
+//       voice_settings: {
+//         stability: 0.5,
+//         similarity_boost: 0.5
+//       }
+//     });
 
-    return new NextResponse(audioStream, {
-      headers: {
-        'Content-Type': 'audio/mpeg',
-      },
-    });
-  } catch (error) {
-    console.error("Eleven Labs API error:", error);
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
-}
+//     return new NextResponse(audioStream, {
+//       headers: {
+//         'Content-Type': 'audio/mpeg',
+//       },
+//     });
+//   } catch (error) {
+//     console.error("Eleven Labs API error:", error);
+//     return NextResponse.json({ error: error.message }, { status: 500 });
+//   }
+// }
 
 
 
